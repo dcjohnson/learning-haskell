bcedf2e53f07962de9c8430079e2366a97807368
Dylan Johnson <djohnson@ancestry.com>
2016-08-31 22:44:44 -0600

    figured out arrow notation

---
+++ arrow_notation.hs
@@ -0,0 +1,17 @@
+main :: IO ()
+main = do
+  print $ get test
+
+  
+test :: Maybe Integer
+test = do
+  x <- may 6
+  may x
+
+
+may :: Integer -> Maybe Integer
+may i = Just i
+
+get :: Maybe Integer -> Integer
+get Nothing = 0
+get (Just i) = i
