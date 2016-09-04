c8acc483afc441b4d137fcd680e0604842c713f4
Dylan Johnson <djohnson@ancestry.com>
2016-09-04 14:02:07 -0600

    update

--- project_0.hs
+++ project_0.hs
@@ -1,5 +1,6 @@
 import Data.Word
 import Data.Bits
+import Data.Maybe
 import qualified Data.ByteString as Bs
 import qualified System.IO as Io
 
@@ -16,10 +17,12 @@ main = do
   print $ ((shiftL (fromIntegral (defaultTestMemory1 !! 2)) 8) :: Word32)
   print $ (Bs.pack [65 | _ <- [1..10]])
   print $ Bs.split 65 (Bs.pack abString)
-  print $ readWord32FromMemory defaultTestMemory1 0
+  print $ fromJust $ readWord32FromMemory defaultTestMemory1 20
   memory <- readFileToMemory "test.txt"
   Bs.putStr $ Bs.pack memory
-  print $ readWord32FromMemory memory 11
+  print $ fromJust $ readWord32FromMemory memory 11
+  let w = safeIndex defaultTestMemory1 8
+  print $ if isJust w then fromJust w else 0
 
 readFileToMemory :: String -> IO Memory
 readFileToMemory path = do
@@ -32,10 +35,16 @@ readFileToMemory path = do
 byteStringToMemory :: Bs.ByteString -> Memory
 byteStringToMemory b = Bs.unpack b
 
-readWord32FromMemory :: Memory -> Int -> Word32
+readWord32FromMemory :: Memory -> Int -> Maybe Word32
 readWord32FromMemory m i = do
-  let w0 = shiftL (fromIntegral (m !! i)) 24
-      w1 = shiftL (fromIntegral (m !! (i + 1))) 16
-      w2 = shiftL (fromIntegral (m !! (i + 2))) 8
-      w3 = fromIntegral (m !! (i + 3))
-      in w0 .|. (w1 .|. (w2 .|. w3))
+  if (i + 3) >= (length m) then Nothing
+  else let w0 = shiftL (fromIntegral (m !! i)) 24
+           w1 = shiftL (fromIntegral (m !! (i + 1))) 16
+           w2 = shiftL (fromIntegral (m !! (i + 2))) 8
+           w3 = fromIntegral (m !! (i + 3))
+           in Just $ w0 .|. (w1 .|. (w2 .|. w3))
+
+safeIndex :: Memory -> Int -> Maybe Word8
+safeIndex m i = do
+  if ((i < 0) || (i >= (length m))) then Nothing
+  else Just $ m !! i
